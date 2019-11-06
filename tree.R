493efdeceb7459cb5bd8a53b81dae195cd4e678e
Yili Zhang <yili.zhang@northwestern.edu>
2019-11-06 14:56:11 -0600

    commit tree.R

--- tree.R
+++ tree.R
@@ -9,11 +9,22 @@ names(df)
 dim(df)
 feq <- table(df$CCN)
 
+table(df$type)
+
+levels(df$type)
+
+levels(df$type) <- c('1','2','3','4','5','6','7','8','9','10','11','12','13')
+
+levels(df$type)
+
+table(df$type)
+
 income <- read.table("MedianIncome.csv",head=TRUE,sep=",",colClasses=c('factor','integer','character','integer'))
 
 income$Mean[income$Mean=="."] <- NA
 
 income$Mean <- as.integer(income$Mean)
+
 
 merg <- merge(df,income,by="Zip")
 ruca <- read.csv("RUCAfile.csv",head=TRUE,sep=",",colClasses=c('factor','factor','double','double','factor'))
@@ -25,6 +36,7 @@ merg$RUCA <- floor(merg$RUCA)
 names(merg)
 head(merg)
 dim(merg)
+
 
 data <- merg[,c(-7:-4,-2,-1,-15,-17,-19,-21,-23,-25,-27,-29,-31,-33,-35,-37,-39,-41,-43,-45,-47,-48)]
 
@@ -51,41 +63,6 @@ dataRural <- fulldata[fulldata$RUCA=='Rural',]
 dataUrban <- fulldata[fulldata$RUCA=='Urban',]
 
 
-model0 <- rpart(formula= admitted ~ timely + taughtdrugs + checkfall + checkdepression + taughtfootcare + betterwalking + betterbed + betterbathing + lesspain + betterbreathing + betterheal + betterdrug + RUCA, data =fulldata,cp=0.005)
-rpart.plot(model0,digits=8,fallen.leaves=TRUE,type=4,extra=5)
-
-model1 <- rpart(formula= admitted ~ timely + taughtdrugs + checkfall + checkdepression + taughtfootcare + RUCA, data =fulldata,cp=0.0098)
-summary(model1)
-rpart.plot(model1,digits=4,fallen.leaves=TRUE,type=4,extra=1)
-
-
-model2 <- rpart(formula= urgent ~ timely + taughtdrugs + checkfall + checkdepression + taughtfootcare + RUCA, data =fulldata,cp=0.012)
-summary(model2)
-rpart.plot(model2,digits=4,fallen.leaves=TRUE,type=4,extra=1)
-
-
-par(mfrow=c(2,2))
-
-model3 <- rpart(formula= admitted ~ timely + taughtdrugs + checkfall + checkdepression + taughtfootcare, data =dataUrban,cp=0.005)
-summary(model3)
-rpart.plot(model3,digits=4,fallen.leaves=TRUE,type=4,extra=1)
-
-
-model4 <- rpart(formula= urgent ~ timely + taughtdrugs + checkfall + checkdepression + taughtfootcare, data =dataUrban,cp=0.008)
-summary(model4)
-rpart.plot(model4,digits=4,fallen.leaves=TRUE,type=4,extra=1)
-
-
-model5 <- rpart(formula= admitted ~ timely + taughtdrugs + checkfall + checkdepression + taughtfootcare, data =dataRural,cp=0.003)
-summary(model5)
-rpart.plot(model5,digits=4,fallen.leaves=TRUE,type=4,extra=1)
-
-
-model6 <- rpart(formula= urgent ~ timely + taughtdrugs + checkfall + checkdepression + taughtfootcare, data =dataRural,cp=0.0053)
-summary(model6)
-rpart.plot(model6,digits=4,fallen.leaves=TRUE,type=4,extra=1)
-
-
 
 
 
@@ -96,13 +73,13 @@ summary(model1)
 rpart.plot(model1,digits=4,fallen.leaves=TRUE,type=4,extra=1)
 
 
-model2 <- rpart(formula= urgent ~ type + off.nursing + off.physical + off.occupational + off.speech + off.medical + off.hha + timely + taughtdrugs + checkfall + checkdepression + taughtfootcare + RUCA, data =fulldata,cp=0.005)
+model2 <- rpart(formula= urgent ~ type + off.nursing + off.physical + off.occupational + off.speech + off.medical + off.hha + timely + taughtdrugs + checkfall + checkdepression + taughtfootcare + RUCA, data =fulldata,cp=0.01)
 summary(model2)
 rpart.plot(model2,digits=4,fallen.leaves=TRUE,type=4,extra=1)
 
 par(mfrow=c(2,2))
 
-model3 <- rpart(formula= admitted ~ type + off.nursing + off.physical + off.occupational + off.speech + off.medical + off.hha + timely + taughtdrugs + checkfall + checkdepression + taughtfootcare, data =dataUrban,cp=0.005)
+model3 <- rpart(formula= admitted ~ type + off.nursing + off.physical + off.occupational + off.speech + off.medical + off.hha + timely + taughtdrugs + checkfall + checkdepression + taughtfootcare, data =dataUrban,cp=0.0075)
 summary(model3)
 rpart.plot(model3,digits=4,fallen.leaves=TRUE,type=4,extra=1)
 
@@ -112,11 +89,48 @@ summary(model4)
 rpart.plot(model4,digits=4,fallen.leaves=TRUE,type=4,extra=1)
 
 
-model5 <- rpart(formula= admitted ~ type + off.nursing + off.physical + off.occupational + off.speech + off.medical + off.hha + timely + taughtdrugs + checkfall + checkdepression + taughtfootcare, data =dataRural,cp=0.002)
+model5 <- rpart(formula= admitted ~ type + off.nursing + off.physical + off.occupational + off.speech + off.medical + off.hha + timely + taughtdrugs + checkfall + checkdepression + taughtfootcare, data =dataRural,cp=0.003)
 summary(model5)
 rpart.plot(model5,digits=4,fallen.leaves=TRUE,type=4,extra=1)
 
 
-model6 <- rpart(formula= urgent ~ type + off.nursing + off.physical + off.occupational + off.speech + off.medical + off.hha + timely + taughtdrugs + checkfall + checkdepression + taughtfootcare, data =dataRural,cp=0.006)
+model6 <- rpart(formula= urgent ~ type + off.nursing + off.physical + off.occupational + off.speech + off.medical + off.hha + timely + taughtdrugs + checkfall + checkdepression + taughtfootcare, data =dataRural,cp=0.005)
 summary(model6)
 rpart.plot(model6,digits=4,fallen.leaves=TRUE,type=4,extra=1)
+
+
+
+## no characteristics
+## model0 <- rpart(formula= admitted ~ timely + taughtdrugs + checkfall + checkdepression + taughtfootcare + betterwalking + betterbed + betterbathing + lesspain + betterbreathing + betterheal + betterdrug + RUCA, data =fulldata,cp=0.005)
+## rpart.plot(model0,digits=8,fallen.leaves=TRUE,type=4)
+
+## model1 <- rpart(formula= admitted ~ timely + taughtdrugs + checkfall + checkdepression + taughtfootcare + RUCA, data =fulldata,cp=0.0098)
+## summary(model1)
+## rpart.plot(model1,digits=4,fallen.leaves=TRUE,type=4,extra=1)
+
+
+## model2 <- rpart(formula= urgent ~ timely + taughtdrugs + checkfall + checkdepression + taughtfootcare + RUCA, data =fulldata,cp=0.012)
+## summary(model2)
+## rpart.plot(model2,digits=4,fallen.leaves=TRUE,type=4,extra=1)
+
+
+## par(mfrow=c(2,2))
+
+## model3 <- rpart(formula= admitted ~ timely + taughtdrugs + checkfall + checkdepression + taughtfootcare, data =dataUrban,cp=0.005)
+## summary(model3)
+## rpart.plot(model3,digits=4,fallen.leaves=TRUE,type=4,extra=1)
+
+
+## model4 <- rpart(formula= urgent ~ timely + taughtdrugs + checkfall + checkdepression + taughtfootcare, data =dataUrban,cp=0.008)
+## summary(model4)
+## rpart.plot(model4,digits=4,fallen.leaves=TRUE,type=4,extra=1)
+
+
+## model5 <- rpart(formula= admitted ~ timely + taughtdrugs + checkfall + checkdepression + taughtfootcare, data =dataRural,cp=0.003)
+## summary(model5)
+## rpart.plot(model5,digits=4,fallen.leaves=TRUE,type=4,extra=1)
+
+
+## model6 <- rpart(formula= urgent ~ timely + taughtdrugs + checkfall + checkdepression + taughtfootcare, data =dataRural,cp=0.0053)
+## summary(model6)
+## rpart.plot(model6,digits=4,fallen.leaves=TRUE,type=4,extra=1)
