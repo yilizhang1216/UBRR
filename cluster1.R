5c491c4f251902614aa0ae5c6a70163384a04cd3
Yili Zhang <yili.zhang@northwestern.edu>
2020-06-16 03:04:32 -0400

    cluster 1 for median, pop, episode, ruca clustering

--- cluster1.R
+++ cluster1.R
@@ -8,12 +8,14 @@ mytypes <- c('character','factor',rep('character',2),'factor',rep('character',2)
 mynames <- c('CCN','state','name','address','city','Zip','phone','type','off.nursing','off.physical','off.occupational','off.speech','off.medical','off.hha','date','rating','timely','taughtdrugs','checkfall','checkdepression','flushot','pnumococcal','taughtfootcare','betterwalking','betterbed','betterbathing','lesspain','betterbreathing','betterheal','betterdrug','admitted','ER','episode','year','season','timeindex','median','mean','pop','ruca')
 df <- read.csv("input/HHA2010-2019.csv",sep=',',colClasses=mytypes)
 dim(df)
-names(df)
+names(df) 
 
-data <- df[,c(-16,-32)] ###rating is missing in 20101-20152 ER is missing in 20104-20123
+#data <- df[,c(-16,-32)] ###rating is missing in 20101-20152 ER is missing in 20104-20123
+data <- df[,c(-16,-32,-33)] ###rating is missing in 20101-20152 ER is missing in 20104-20123, also removed ER because its missing in 2018
 data[data=='Y'] <- 'Yes'
 data[data=='N'] <- 'No'
 data[data==199|data==201]<-NA
+data[data==5030353|data==16665138]<-NA
 
 fulldata <- na.omit(data)
 dim(fulldata)
@@ -29,79 +31,25 @@ lm.model0 <- lm(admitted ~ timely + taughtdrugs + checkfall + checkdepression + 
                 data =fulldata)
 summary(lm.model0)
 
+names(fulldata)
+#names(fulldata)[c(31,35,37,38)]
+names(fulldata)[c(34,36:37)]
 
-## cluster by episode, median, pop,ruca
 set.seed(1)
-fit1 <- kmeans(fulldata[,c(31,36:38)], 2, nstart = 25)
+#fit1 <- kmeans(fulldata[,c(31,35,37,38)], 9, nstart = 25)
+fit1 <- kmeans(fulldata[,c(34,36:37)], 9, nstart = 25)
 table(fit1$cluster)
 fit1.data <- cbind(fulldata, cluster = fit1$cluster)
 
 cluster1 <- fit1.data[which(fit1.data$cluster=='1'),]
 cluster2 <- fit1.data[which(fit1.data$cluster=='2'),]
-
-
-### cluster 2 is high episode,median,pop & low ruca (urban area)
-summary(cluster1[,c(31,36:38)])
-summary(cluster2[,c(31,36:38)])
-
-
-mean1 <- c(mean(cluster1$episode),mean(cluster1$median),mean(cluster1$pop),mean(cluster1$ruca))
-mean2 <- c(mean(cluster2$episode),mean(cluster2$median),mean(cluster2$pop),mean(cluster2$ruca))
-mean1
-mean2
-
-lm.model1 <- lm(admitted ~ timely + taughtdrugs + checkfall + checkdepression + taughtfootcare
-               + type 
-               + off.physical + off.occupational + off.speech + off.medical + off.hha 
-               + betterwalking + betterbed + betterbathing + lesspain + betterbreathing + betterheal + betterdrug 
-               + flushot + pnumococcal
-               ,
-               data =cluster1)
-summary(lm.model1)
-
-lm.model2 <- lm(admitted ~ timely + taughtdrugs + checkfall + checkdepression + taughtfootcare
-                + type 
-                + off.physical + off.occupational + off.speech + off.medical + off.hha 
-                + betterwalking + betterbed + betterbathing + lesspain + betterbreathing + betterheal + betterdrug 
-                + flushot + pnumococcal
-                ,
-                data =cluster2)
-summary(lm.model2)
-
-
-
-print(fit1)
-# Centroid Plot against 1st 2 discriminant functions
-plotcluster(fulldata[,c(31,36:38)], fit1$cluster)
-
-
-
-
-
-## cluster by outcome and utilization variables but no ER
-## select the best number of cluster
-set.seed(0)
-fviz_nbclust(fulldata[,c(23:30)], kmeans, nstart = 25,  method = "gap_stat", nboot = 1)+labs(subtitle = "Gap statistic method")
-
-
-set.seed(2)
-fit2 <- kmeans(fulldata[,c(23:30)], 2, nstart = 25)
-table(fit2$cluster)
-print(fit2)
-
-fit2.data <- cbind(fulldata, cluster = fit2$cluster)
-cluster1 <- fit2.data[which(fit2.data$cluster=='1'),]
-cluster2 <- fit2.data[which(fit2.data$cluster=='2'),]
-
-### cluster 2 is high episode,median,pop & low ruca (urban area)
-summary(cluster1[,c(23:30)])
-summary(cluster2[,c(23:30)])
-
-
-mean1 <- c(mean(cluster1$betterwalking),mean(cluster1$betterbed),mean(cluster1$betterbathing),mean(cluster1$lesspain),mean(cluster1$betterbreathing),mean(cluster1$betterheal),mean(cluster1$betterdrug),mean(cluster1$admitted))
-mean2 <- c(mean(cluster2$betterwalking),mean(cluster2$betterbed),mean(cluster2$betterbathing),mean(cluster2$lesspain),mean(cluster2$betterbreathing),mean(cluster2$betterheal),mean(cluster2$betterdrug),mean(cluster2$admitted))
-mean1
-mean2
+cluster3 <- fit1.data[which(fit1.data$cluster=='3'),]
+cluster4 <- fit1.data[which(fit1.data$cluster=='4'),]
+cluster5 <- fit1.data[which(fit1.data$cluster=='5'),]
+cluster6 <- fit1.data[which(fit1.data$cluster=='6'),]
+cluster7 <- fit1.data[which(fit1.data$cluster=='7'),]
+cluster8 <- fit1.data[which(fit1.data$cluster=='8'),]
+cluster9 <- fit1.data[which(fit1.data$cluster=='9'),]
 
 
 lm.model1 <- lm(admitted ~ timely + taughtdrugs + checkfall + checkdepression + taughtfootcare
@@ -122,14 +70,56 @@ lm.model2 <- lm(admitted ~ timely + taughtdrugs + checkfall + checkdepression + 
                 data =cluster2)
 summary(lm.model2)
 
+lm.model3 <- lm(admitted ~ timely + taughtdrugs + checkfall + checkdepression + taughtfootcare
+                + type 
+                + off.physical + off.occupational + off.speech + off.medical + off.hha 
+                + betterwalking + betterbed + betterbathing + lesspain + betterbreathing + betterheal + betterdrug 
+                + flushot + pnumococcal
+                ,
+                data =cluster3)
+summary(lm.model3)
+
+lm.model4 <- lm(admitted ~ timely + taughtdrugs + checkfall + checkdepression + taughtfootcare
+                + type 
+                + off.physical + off.occupational + off.speech + off.medical + off.hha 
+                + betterwalking + betterbed + betterbathing + lesspain + betterbreathing + betterheal + betterdrug 
+                + flushot + pnumococcal
+                ,
+                data =cluster4)
+summary(lm.model4)
+
+lm.model5 <- lm(admitted ~ timely + taughtdrugs + checkfall + checkdepression + taughtfootcare
+                + type 
+                + off.physical + off.occupational + off.speech + off.medical + off.hha 
+                + betterwalking + betterbed + betterbathing + lesspain + betterbreathing + betterheal + betterdrug 
+                + flushot + pnumococcal
+                ,
+                data =cluster5)
+summary(lm.model5)
+
+
+
+
+
+
+
+### cluster 2 is high episode,median,pop & low ruca (urban area)
+summary(cluster1[,c(31,35,37,38)])
+summary(cluster2[,c(31,35,37,38)])
+summary(cluster3[,c(31,35,37,38)])
+#summary(cluster1[,c(34,36:37)])
+#summary(cluster2[,c(34,36:37)])
+
+
+mean1 <- c(mean(cluster1$episode),mean(cluster1$median),mean(cluster1$pop),mean(cluster1$ruca))
+mean2 <- c(mean(cluster2$episode),mean(cluster2$median),mean(cluster2$pop),mean(cluster2$ruca))
+#mean1 <- c(mean(cluster1$median),mean(cluster1$pop),mean(cluster1$ruca))
+#mean2 <- c(mean(cluster2$median),mean(cluster2$pop),mean(cluster2$ruca))
+mean1
+mean2
+
+
+
+print(fit1)
 # Centroid Plot against 1st 2 discriminant functions
-plotcluster(fulldata[,c(31,36:38)], fit2$cluster)
-
-
-
-
-
-
-
-
-
+plotcluster(fulldata[,c(31,36:38)], fit1$cluster)
