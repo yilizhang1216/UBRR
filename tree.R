8e95683457671ec8cad0ee73ea8c41ad9660faa7
Yili Zhang <yili.zhang@northwestern.edu>
2020-06-18 16:01:05 -0400

    added timeindex to model

--- tree.R
+++ tree.R
@@ -5,30 +5,32 @@ library(tree)
 library(dplyr)
 
 
-mytypes <- c('character','factor',rep('character',2),'factor',rep('character',2),rep('factor',7),'character',rep('numeric',18),rep('factor',3),rep('numeric',4))
+mytypes <- c('character','factor',rep('character',2),'factor',rep('character',2),rep('factor',7),'character',rep('numeric',18),rep('factor',2),rep('numeric',5))
 mynames <- c('CCN','state','name','address','city','Zip','phone','type','off.nursing','off.physical','off.occupational','off.speech','off.medical','off.hha','date','rating','timely','taughtdrugs','checkfall','checkdepression','flushot','pnumococcal','taughtfootcare','betterwalking','betterbed','betterbathing','lesspain','betterbreathing','betterheal','betterdrug','admitted','ER','episode','year','season','timeindex','median','mean','pop','ruca')
 df <- read.csv("input/HHA2010-2019.csv",sep=',',colClasses=mytypes)
 dim(df)
 names(df)
 
-#data <- filter(df, df$timeindex %in% c(4:12))
-data <- filter(df, df$timeindex %in% c(13:40))
+data <- filter(df, df$timeindex %in% c(1:40))
+#data <- filter(df, df$timeindex %in% c(13:40))
 names(data) 
 
-#data_bf <- data.frame(data[,c(8:14)],data[,c(17:32)],data[,c(34:37)],data[,39:40]) ## no episode, no mean, no rating
-data_bf <- data.frame(data[,c(8:14)],data[,c(17:31)],data[,c(34:40)])
-#names(data_bf)[26] <- 'ruca'
+#data_bf <- data.frame(data[,c(8:14)],data[,c(17:23)],data[,c(31:40)])
+data_bf <- data.frame(data[,c(17:20)],data[,c(23:23)],data[,c(31:40)])
+names(data_bf)[5] <- 'taughtfootcare'
 names(data_bf)
 dim(data_bf)
 sum(is.na(data_bf))
 
 
 data_bf[data_bf==199|data_bf==201]<-NA
+data_bf[data_bf==5030353|data_bf==16665138]<-NA
 sum(is.na(data_bf))
 
 table(data_bf$timeindex)
 table(data_bf$year)
 
+#fulldata <- na.omit(data_bf$admitted)
 fulldata <- na.omit(data_bf)
 dim(fulldata)
 
@@ -47,19 +49,38 @@ dataUrban <- fulldata[fulldata$ruca=='Urban',]
 
 
 model1 <- rpart(formula= admitted ~ timely + taughtdrugs + checkfall + checkdepression + taughtfootcare 
-                + betterwalking + betterbed + betterbathing + lesspain + betterbreathing + betterheal + betterdrug 
-                + median + pop
+                #+ betterwalking + betterbed + betterbathing + lesspain + betterbreathing + betterheal + betterdrug 
+                + median 
+                #+ pop
+                + episode
                 + ruca
-                + flushot + pnumococcal
+                + timeindex
+                #+ flushot + pnumococcal
                 , 
-                data =fulldata,cp=0.003
+                model=TRUE,
+                data =fulldata,cp=0.002
                 )
 summary(model1)
 rpart.plot(model1,digits=4,fallen.leaves=TRUE,type=4,extra=1)
 
-table(fulldata$timeindex)
+model2 <- rpart(formula= ER ~ timely + taughtdrugs + checkfall + checkdepression + taughtfootcare 
+                #+ betterwalking + betterbed + betterbathing + lesspain + betterbreathing + betterheal + betterdrug 
+                + median 
+                #+ pop
+                + episode
+                + ruca
+                + timeindex
+                #+ flushot + pnumococcal
+                , 
+                model=TRUE,
+                data =fulldata,cp=0.0018
+)
+summary(model2)
+rpart.plot(model2,digits=4,fallen.leaves=TRUE,type=4,extra=1)
+
+
 table(fulldata$year)
-table(fulldata$ruca)
+table(fulldata$timeindex)
 
 
 
