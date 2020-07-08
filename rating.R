3451df887072971ee697352dbc1b3955b7c583bb
Yili Zhang <yili.zhang@northwestern.edu>
2020-07-08 17:43:27 -0400

    changed code for better presentation and discussion

--- rating.R
+++ rating.R
@@ -1,3 +1,10 @@
+library("nnet")
+library(foreign)
+library(ggplot2)
+library(MASS)
+library(Hmisc)
+library(reshape2)
+
 mytypes <- c('character','factor',rep('character',2),'factor',rep('character',2),rep('factor',7),'character',rep('numeric',18),rep('factor',2),rep('numeric',6))
 mynames <- c('CCN','state','name','address','city','Zip','phone','type','off.nursing','off.physical','off.occupational','off.speech','off.medical','off.hha','date','rating','timely','taughtdrugs','checkfall','checkdepression','flushot','pnumococcal','taughtfootcare','betterwalking','betterbed','betterbathing','lesspain','betterbreathing','betterheal','betterdrug','admitted','ER','episode','star','year','season','timeindex','median','mean','pop','ruca')
 df <- read.csv("input/HHA2010-2019.csv",sep=',',colClasses=mytypes)
@@ -16,12 +23,48 @@ names(data)[27] <- 'ruca'
 data[data==199|data==201]<-NA
 data[data==5030353|data==16665138]<-NA
 names(data)
+dim(data)
 #fulldata <- na.omit(data)
+data$star[data$star %in% c('','Not Available')] <- NA
+table(data$star)
+data_1 <- na.omit(data)
+table(data_1$star)
+
 data_1 <- data[which(data$year %in% c(2016,2017,2018,2019)),]
-fulldata <- data_1
+data_2 <- data_1[which(data_1$star %in% c(1,2,3,4,5)),]
+data_3 <- na.omit(data_2)
+fulldata <- data_3
 dim(fulldata)
 table(fulldata$year)
+table(fulldata$star)
+fulldata$star <- factor(fulldata$star, levels = c("1", "2", "3", "4", "5"))
+table(fulldata$star)
 
+olr.model<- polr(star ~ timely + taughtdrugs + checkfall + checkdepression + taughtfootcare
+         #+ type + off.physical + off.occupational + off.speech + off.medical + off.hha 
+         #+ betterwalking + betterbed + betterbathing + lesspain + betterbreathing + betterheal + betterdrug 
+         #+ admitted + ER 
+         + log1p(episode)
+         #+ timeindex
+         #+ median 
+         #+ pop
+         + ruca
+         #+ flushot + pnumococcal
+         ,
+         data =fulldata,
+         Hess=TRUE)
+summary(olr.model)
+
+ctable <- coef(summary(olr.model))
+p <- pnorm(abs(ctable[, "t value"]), lower.tail = FALSE) * 2
+ctable <- cbind(ctable, "p value" = p)
+ctable
+ci <- confint(olr.model)
+ci
+exp(coef(olr.model))
+exp(cbind(OR = coef(olr.model), ci))
+
+fulldata$star <- as.numeric(fulldata$star)
 
 lm.model <- lm(star ~ timely + taughtdrugs + checkfall + checkdepression + taughtfootcare
                #+ type + off.physical + off.occupational + off.speech + off.medical + off.hha 
--- tree.R
+++ tree.R
@@ -11,40 +11,36 @@ df <- read.csv("input/HHA2010-2019.csv",sep=',',colClasses=mytypes)
 dim(df)
 names(df)
 
-data <- filter(df, df$timeindex %in% c(1:40))
-#data <- filter(df, df$timeindex %in% c(13:40))
-names(data) 
+df$ruca[df$ruca==1] <- "Urban"
+df$ruca[df$ruca!='Urban'] <- "Rural"
+table(df$ruca)
+dummyruca <- as.factor(df$ruca)
+df$ruca <- dummyruca
 
-#data_bf <- data.frame(data[,c(8:14)],data[,c(17:23)],data[,c(31:40)])
-data_bf <- data.frame(data[,c(17:20)],data[,c(23:23)],data[,c(31:33)],data[,c(35:41)])
-names(data_bf)[5] <- 'taughtfootcare'
-names(data_bf)
-dim(data_bf)
-sum(is.na(data_bf))
+df[df==199|df==201]<-NA
+df[df==5030353|df==16665138]<-NA
+sum(is.na(df))
+
+names(df)
+sum(is.na(df))
+
+data_admitted <- data.frame(df[,c(17:20)],df[,c(23,31,33)],df[,c(35:41)])
+names(data_admitted)
+data_ER <- data.frame(df[,c(17:20)],df[,c(23,32,33)],df[,c(35:41)])
+names(data_ER)
+
+dim(data_admitted)
+sum(is.na(data_admitted))
+dim(data_ER)
+sum(is.na(data_ER))
 
 
-data_bf[data_bf==199|data_bf==201]<-NA
-data_bf[data_bf==5030353|data_bf==16665138]<-NA
-sum(is.na(data_bf))
-
-table(data_bf$timeindex)
-table(data_bf$year)
-
-fulldata <- na.omit(data_bf)
-dim(fulldata)
-
-table(fulldata$timeindex)
-table(fulldata$year)
-
-fulldata$ruca[fulldata$ruca==1] <- "Urban"
-fulldata$ruca[fulldata$ruca!='Urban'] <- "Rural"
-table(fulldata$ruca)
-dummyruca <- as.factor(fulldata$ruca)
-fulldata$ruca <- dummyruca
-
-dataRural <- fulldata[fulldata$ruca=='Rural',]
-dataUrban <- fulldata[fulldata$ruca=='Urban',]
-
+fulldata_admitted <- na.omit(data_admitted)
+dim(fulldata_admitted)
+fulldata_ER <- na.omit(data_ER)
+dim(fulldata_ER)
+table(fulldata_admitted$year)
+table(fulldata_ER$year)
 
 
 model1 <- rpart(formula= admitted ~ timely + taughtdrugs + checkfall + checkdepression + taughtfootcare 
@@ -57,12 +53,12 @@ model1 <- rpart(formula= admitted ~ timely + taughtdrugs + checkfall + checkdepr
                 #+ flushot + pnumococcal
                 , 
                 model=TRUE,
-                data =fulldata,cp=0.002
+                data =fulldata_admitted,cp=0.001
                 )
 #0.002
 #0.00019852
 summary(model1)
-rpart.plot(model1,digits=4,fallen.leaves=TRUE,type=4,extra=1)
+rpart.plot(model1,digits=10,fallen.leaves=TRUE,type=4,extra=1)
 printcp(model1)
 rsq.rpart(model1)
 
@@ -77,14 +73,15 @@ model2 <- rpart(formula= ER ~ timely + taughtdrugs + checkfall + checkdepression
                 #+ flushot + pnumococcal
                 , 
                 model=TRUE,
-                data =fulldata,cp=0.0018
+                data =fulldata_ER,cp= 0.0008
+                  #1.6548e-04
 )
 # 0.0018
 # 1.6548e-04
+rpart.plot(model2,digits=4,fallen.leaves=TRUE,type=4,extra=1)
 summary(model2)
 printcp(model2)
 rsq.rpart(model2)
-rpart.plot(model2,digits=4,fallen.leaves=TRUE,type=4,extra=1)
 
 
 table(fulldata$year)
