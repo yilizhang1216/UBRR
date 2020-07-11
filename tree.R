68147be123acfbc2a3452acaf0cf93f75e569b7f
Yili Zhang <yili.zhang@northwestern.edu>
2020-07-11 02:25:16 -0400

    added CI for each node

--- tree.R
+++ tree.R
@@ -63,8 +63,22 @@ rpart.plot(model1,digits=10,fallen.leaves=TRUE,type=4,extra=1)
 printcp(model1)
 rsq.rpart(model1)
 
+round(CI(fulldata_admitted$admitted,0.95)[1]-CI(fulldata_admitted$admitted,0.95)[2],2)
+round(CI(fulldata_admitted[fulldata_admitted$timeindex>=13,]$admitted,0.95)[1]-CI(fulldata_admitted[fulldata_admitted$timeindex>=13,]$admitted,0.95)[2],2)
+round(CI(fulldata_admitted[fulldata_admitted$timeindex<13,]$admitted,0.95)[1]-CI(fulldata_admitted[fulldata_admitted$timeindex<13,]$admitted,0.95)[2],2)
+round(CI(fulldata_admitted[fulldata_admitted$timeindex>=13 & fulldata_admitted$ruca == 'Urban' ,]$admitted,0.95)[1]-CI(fulldata_admitted[fulldata_admitted$timeindex>=13 & fulldata_admitted$ruca == 'Urban',]$admitted,0.95)[2],2)
+round(CI(fulldata_admitted[fulldata_admitted$timeindex>=13 & fulldata_admitted$ruca == 'Rural' ,]$admitted,0.95)[1]-CI(fulldata_admitted[fulldata_admitted$timeindex>=13 & fulldata_admitted$ruca == 'Rural',]$admitted,0.95)[2],2)
+round(CI(fulldata_admitted[fulldata_admitted$timeindex>=13 & fulldata_admitted$ruca == 'Urban' & fulldata_admitted$episode <636.5,]$admitted,0.95)[1]-CI(fulldata_admitted[fulldata_admitted$timeindex>=13 & fulldata_admitted$ruca == 'Urban' & fulldata_admitted$episode <636.5,]$admitted,0.95)[2],2)
+round(CI(fulldata_admitted[fulldata_admitted$timeindex>=13 & fulldata_admitted$ruca == 'Urban' & fulldata_admitted$episode >=636.5,]$admitted,0.95)[1]-CI(fulldata_admitted[fulldata_admitted$timeindex>=13 & fulldata_admitted$ruca == 'Urban' & fulldata_admitted$episode >=636.5,]$admitted,0.95)[2],2)
+round(CI(fulldata_admitted[fulldata_admitted$timeindex<13 & fulldata_admitted$timely >= 96.5,]$admitted,0.95)[1]-CI(fulldata_admitted[fulldata_admitted$timeindex<13 & fulldata_admitted$timely >= 96.5,]$admitted,0.95)[2],2)
+round(CI(fulldata_admitted[fulldata_admitted$timeindex<13 & fulldata_admitted$timely < 96.5,]$admitted,0.95)[1]-CI(fulldata_admitted[fulldata_admitted$timeindex<13 & fulldata_admitted$timely < 96.5,]$admitted,0.95)[2],2)
+round(CI(fulldata_admitted[fulldata_admitted$timeindex<13 & fulldata_admitted$timely >= 96.5 & fulldata_admitted$ruca == 'Urban',]$admitted,0.95)[1]-CI(fulldata_admitted[fulldata_admitted$timeindex<13 & fulldata_admitted$timely >= 96.5 & fulldata_admitted$ruca == 'Urban',]$admitted,0.95)[2],2)
+round(CI(fulldata_admitted[fulldata_admitted$timeindex<13 & fulldata_admitted$timely >= 96.5 & fulldata_admitted$ruca == 'Rural',]$admitted,0.95)[1]-CI(fulldata_admitted[fulldata_admitted$timeindex<13 & fulldata_admitted$timely >= 96.5 & fulldata_admitted$ruca == 'Rural',]$admitted,0.95)[2],2)
+round(CI(fulldata_admitted[fulldata_admitted$timeindex<13 & fulldata_admitted$timely < 96.5 & fulldata_admitted$ruca == 'Urban',]$admitted,0.95)[1]-CI(fulldata_admitted[fulldata_admitted$timeindex<13 & fulldata_admitted$timely < 96.5 & fulldata_admitted$ruca == 'Urban',]$admitted,0.95)[2],2)
+round(CI(fulldata_admitted[fulldata_admitted$timeindex<13 & fulldata_admitted$timely < 96.5 & fulldata_admitted$ruca == 'Rural',]$admitted,0.95)[1]-CI(fulldata_admitted[fulldata_admitted$timeindex<13 & fulldata_admitted$timely < 96.5 & fulldata_admitted$ruca == 'Rural',]$admitted,0.95)[2],2)
 
-
+CI(fulldata_admitted[fulldata_admitted$timeindex<13 & fulldata_admitted$timely < 96.5 & fulldata_admitted$ruca == 'Urban',]$admitted,0.95)
+±
 
 model2 <- rpart(formula= ER ~ timely + taughtdrugs + checkfall + checkdepression + taughtfootcare 
                 #+ betterwalking + betterbed + betterbathing + lesspain + betterbreathing + betterheal + betterdrug 
@@ -81,10 +95,30 @@ model2 <- rpart(formula= ER ~ timely + taughtdrugs + checkfall + checkdepression
 )
 # 0.0018
 # 1.6548e-04
-rpart.plot(model2,digits=4,fallen.leaves=TRUE,type=4,extra=1)
+rpart.plot(model2,digits=10,fallen.leaves=TRUE,type=4,extra=1)
 summary(model2)
 printcp(model2)
 rsq.rpart(model2)
+
+round(CI(fulldata_ER$ER,0.95)[1]-CI(fulldata_ER$ER,0.95)[2],2)
+round(CI(fulldata_ER[fulldata_ER$ruca == 'Urban',]$ER,0.95)[1]-CI(fulldata_ER[fulldata_ER$ruca == 'Urban',]$ER,0.95)[2],2)
+round(CI(fulldata_ER[fulldata_ER$ruca == 'Urban' & fulldata_ER$checkdepression >=99.95,]$ER,0.95)[1]-CI(fulldata_ER[fulldata_ER$ruca == 'Urban' & fulldata_ER$checkdepression >=99.95,]$ER,0.95)[2],2)
+round(CI(fulldata_ER[fulldata_ER$ruca == 'Urban' & fulldata_ER$checkdepression >=99.95 & fulldata_ER$timeindex <27,]$ER,0.95)[1]-CI(fulldata_ER[fulldata_ER$ruca == 'Urban' & fulldata_ER$checkdepression >=99.95 & fulldata_ER$timeindex <27,]$ER,0.95)[2],2)
+round(CI(fulldata_ER[fulldata_ER$ruca == 'Urban' & fulldata_ER$checkdepression >=99.95 & fulldata_ER$timeindex >=27,]$ER,0.95)[1]-CI(fulldata_ER[fulldata_ER$ruca == 'Urban' & fulldata_ER$checkdepression >=99.95 & fulldata_ER$timeindex >=27,]$ER,0.95)[2],2)
+round(CI(fulldata_ER[fulldata_ER$ruca == 'Urban' & fulldata_ER$checkdepression <99.95,]$ER,0.95)[1]-CI(fulldata_ER[fulldata_ER$ruca == 'Urban' & fulldata_ER$checkdepression <99.95,]$ER,0.95)[2],2)
+round(CI(fulldata_ER[fulldata_ER$ruca == 'Urban' & fulldata_ER$checkdepression <99.95 & fulldata_ER$timeindex <24,]$ER,0.95)[1]-CI(fulldata_ER[fulldata_ER$ruca == 'Urban' & fulldata_ER$checkdepression <99.95 & fulldata_ER$timeindex <24,]$ER,0.95)[2],2)
+round(CI(fulldata_ER[fulldata_ER$ruca == 'Urban' & fulldata_ER$checkdepression <99.95 & fulldata_ER$timeindex >=24,]$ER,0.95)[1]-CI(fulldata_ER[fulldata_ER$ruca == 'Urban' & fulldata_ER$checkdepression <99.95 & fulldata_ER$timeindex >=24,]$ER,0.95)[2],2)
+round(CI(fulldata_ER[fulldata_ER$ruca == 'Rural',]$ER,0.95)[1]-CI(fulldata_ER[fulldata_ER$ruca == 'Rural',]$ER,0.95)[2],2)
+round(CI(fulldata_ER[fulldata_ER$ruca == 'Rural' & fulldata_ER$timeindex <24,]$ER,0.95)[1]-CI(fulldata_ER[fulldata_ER$ruca == 'Rural' & fulldata_ER$timeindex <24,]$ER,0.95)[2],2)
+round(CI(fulldata_ER[fulldata_ER$ruca == 'Rural' & fulldata_ER$timeindex <24 & fulldata_ER$median >=57040,]$ER,0.95)[1]-CI(fulldata_ER[fulldata_ER$ruca == 'Rural' & fulldata_ER$timeindex <24 & fulldata_ER$median >=57040,]$ER,0.95)[2],2)
+round(CI(fulldata_ER[fulldata_ER$ruca == 'Rural' & fulldata_ER$timeindex <24 & fulldata_ER$median <57040,]$ER,0.95)[1]-CI(fulldata_ER[fulldata_ER$ruca == 'Rural' & fulldata_ER$timeindex <24 & fulldata_ER$median <57040,]$ER,0.95)[2],2)
+round(CI(fulldata_ER[fulldata_ER$ruca == 'Rural' & fulldata_ER$timeindex >=24,]$ER,0.95)[1]-CI(fulldata_ER[fulldata_ER$ruca == 'Rural' & fulldata_ER$timeindex >=24,]$ER,0.95)[2],2)
+round(CI(fulldata_ER[fulldata_ER$ruca == 'Rural' & fulldata_ER$timeindex >=24 & fulldata_ER$median >=56407,]$ER,0.95)[1]-CI(fulldata_ER[fulldata_ER$ruca == 'Rural' & fulldata_ER$timeindex >=24 & fulldata_ER$median >=56407,]$ER,0.95)[2],2)
+round(CI(fulldata_ER[fulldata_ER$ruca == 'Rural' & fulldata_ER$timeindex >=24 & fulldata_ER$median <56407,]$ER,0.95)[1]-CI(fulldata_ER[fulldata_ER$ruca == 'Rural' & fulldata_ER$timeindex >=24 & fulldata_ER$median <56407,]$ER,0.95)[2],2)
+
+
+CI(fulldata_ER[fulldata_ER$ruca == 'Rural' & fulldata_ER$timeindex >=24,]$ER,0.95)
+
 
 
 table(fulldata$year)
@@ -115,113 +149,3 @@ plot(years,buff.mean,type='l',xlab='year',ylab=names(data)[i])
 lines(years,buff.upper,lty=2)
 lines(years,buff.lower,lty=2)
 }
-
-
-
-
-
-
-buff.mean <- c()
-buff.upper <- c()
-buff.lower <- c()
-for (j in c(2010:2019)) {
-  buff.mean <- c(buff.mean,CI(na.omit(data[data$year==j,]$taughtdrugs))[2])
-  buff.upper <- c(buff.upper,CI(na.omit(data[data$year==j,]$taughtdrugs))[1])
-  buff.lower <- c(buff.lower,CI(na.omit(data[data$year==j,]$taughtdrugs))[3])
-  assign(paste("taughtdrugs","mean", sep = "."), buff.mean)
-  assign(paste("taughtdrugs","upper", sep = "."), buff.upper)
-  assign(paste("taughtdrugs","lower",  sep = "."), buff.lower)
-}
-plot(taughtdrugs.mean,type="l")
-lines(taughtdrugs.upper,lty=2)
-lines(taughtdrugs.lower,lty=2)
-
-buff.mean <- c()
-buff.upper <- c()
-buff.lower <- c()
-for (j in c(2010:2019)) {
-  buff.mean <- c(buff.mean,CI(na.omit(data[data$year==j,]$checkfall))[2])
-  buff.upper <- c(buff.upper,CI(na.omit(data[data$year==j,]$checkfall))[1])
-  buff.lower <- c(buff.lower,CI(na.omit(data[data$year==j,]$checkfall))[3])
-  assign(paste("checkfall","mean", sep = "."), buff.mean)
-  assign(paste("checkfall","upper", sep = "."), buff.upper)
-  assign(paste("checkfall","lower",  sep = "."), buff.lower)
-}
-plot(checkfall.mean,type="l")
-lines(checkfall.upper,lty=2)
-lines(checkfall.lower,lty=2)
-
-buff.mean <- c()
-buff.upper <- c()
-buff.lower <- c()
-for (j in c(2010:2019)) {
-  buff.mean <- c(buff.mean,CI(na.omit(data[data$year==j,]$checkdepression))[2])
-  buff.upper <- c(buff.upper,CI(na.omit(data[data$year==j,]$checkdepression))[1])
-  buff.lower <- c(buff.lower,CI(na.omit(data[data$year==j,]$checkdepression))[3])
-  assign(paste("checkdepression","mean", sep = "."), buff.mean)
-  assign(paste("checkdepression","upper", sep = "."), buff.upper)
-  assign(paste("checkdepression","lower",  sep = "."), buff.lower)
-}
-plot(checkdepression.mean,type="l")
-lines(checkdepression.upper,lty=2)
-lines(checkdepression.lower,lty=2)
-
-buff.mean <- c()
-buff.upper <- c()
-buff.lower <- c()
-for (j in c(2010:2019)) {
-  buff.mean <- c(buff.mean,CI(na.omit(data[data$year==j,]$taughtfootcare))[2])
-  buff.upper <- c(buff.upper,CI(na.omit(data[data$year==j,]$taughtfootcare))[1])
-  buff.lower <- c(buff.lower,CI(na.omit(data[data$year==j,]$taughtfootcare))[3])
-  assign(paste("taughtfootcare","mean", sep = "."), buff.mean)
-  assign(paste("taughtfootcare","upper", sep = "."), buff.upper)
-  assign(paste("taughtfootcare","lower",  sep = "."), buff.lower)
-}
-plot(taughtfootcare.mean,type="l")
-lines(taughtfootcare.upper,lty=2)
-lines(taughtfootcare.lower,lty=2)
-
-buff.mean <- c()
-buff.upper <- c()
-buff.lower <- c()
-for (j in c(2010:2019)) {
-  buff.mean <- c(buff.mean,CI(na.omit(data[data$year==j,]$admitted))[2])
-  buff.upper <- c(buff.upper,CI(na.omit(data[data$year==j,]$admitted))[1])
-  buff.lower <- c(buff.lower,CI(na.omit(data[data$year==j,]$admitted))[3])
-  assign(paste("admitted","mean", sep = "."), buff.mean)
-  assign(paste("admitted","upper", sep = "."), buff.upper)
-  assign(paste("admitted","lower",  sep = "."), buff.lower)
-}
-plot(admitted.mean,type="l")
-lines(admitted.upper,lty=2)
-lines(admitted.lower,lty=2)
-
-buff.mean <- c()
-buff.upper <- c()
-buff.lower <- c()
-for (j in c(2010:2019)) {
-  buff.mean <- c(buff.mean,CI(na.omit(data[data$year==j,]$ER))[2])
-  buff.upper <- c(buff.upper,CI(na.omit(data[data$year==j,]$ER))[1])
-  buff.lower <- c(buff.lower,CI(na.omit(data[data$year==j,]$ER))[3])
-  assign(paste("ER","mean", sep = "."), buff.mean)
-  assign(paste("ER","upper", sep = "."), buff.upper)
-  assign(paste("ER","lower",  sep = "."), buff.lower)
-}
-plot(ER.mean,type="l")
-lines(ER.upper,lty=2)
-lines(ER.lower,lty=2)
-
-buff.mean <- c()
-buff.upper <- c()
-buff.lower <- c()
-for (j in c(2010:2019)) {
-  buff.mean <- c(buff.mean,CI(na.omit(data[data$year==j,]$median))[2])
-  buff.upper <- c(buff.upper,CI(na.omit(data[data$year==j,]$median))[1])
-  buff.lower <- c(buff.lower,CI(na.omit(data[data$year==j,]$median))[3])
-  assign(paste("median","mean", sep = "."), buff.mean)
-  assign(paste("median","upper", sep = "."), buff.upper)
-  assign(paste("median","lower",  sep = "."), buff.lower)
-}
-plot(median.mean,type="l")
-lines(median.upper,lty=2)
-lines(median.lower,lty=2)
